946cd2de3e97d88ab38e570d3e85b6cfb6560435
Anthony Renwick <renwickman@gmail.com>
2023-05-31 13:18:11 -0500

    "commit from vs code"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,23 @@
-import Food from "../icons_assets/Food.jpg"
+import Food_2 from "../icons_assets/Food_2.jpg"
 
 const Header = () => {
  return (
-    <header className="header-background">
-        <div className="left-side">
-            <div className="row">
-                <div className="column">
-            <h1 className="title">Little Lemon</h1>
-            <h1>Chicago</h1>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <button type="button" class="btn btn-warning">
-                Reserve a table
-            </button>
+    <header>
+        <div className="headContainer">
+                <div className="row">
+                    <div className="column">
+                        <h1 className="title">Little Lemon</h1>
+                        <h1 className="title_2">Chicago</h1>
+                        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                        <button type="button" class="btn btn-warning">
+                            Reserve a table
+                        </button>
+                    </div>
+                    <div className="column">
+                        <img width="300px" src={Food_2} alt="person holding food" />
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
-        <div className="right-side">
-            <div className="row">
-                <div className="column">
-            <img 
-            width="300px"
-            src={Food}
-            alt="person holding food"
-            />
-            </div>
-            </div>
-        </div>
     </header>
  )
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,82 +5,87 @@ import reviewer4 from "../icons_assets/reviewer4.jpg";
 import greek_salad from "../icons_assets/greek_salad.jpg";
 import Mario_and_Adrian_A from "../icons_assets/Mario_Adrian_A.jpg";
 import Mario_and_Adrian_B from "../icons_assets/Mario_Adrian_B.jpg";
-import { MdOutlineDeliveryDining } from "react-icons/md";
 import lemon_dessert from "../icons_assets/lemon_dessert.jpg";
 import bruschetta from "../icons_assets/bruschetta.svg";
+// import { BsFillCarFrontFill } from  "react-icons/bs";
 
 
 
 
 function Main() {
     return  (
-    <main className="specials-container">
+    <main>
         <section id="weekSpecial">
-        <div className="top-part">
-            <h1>This week's specials!</h1>
-            <button class="btn btn-warning">Online Menu</button>
-        </div>
-        <div className="content">
-            <div className="row">
-                <div className="col">
-                    <div className="card">
-                        <img src={greek_salad} alt="cardImage" className="cardImage" />
-                        <div className="cardContent">
-                            <div className="cardHeading">
-                                <h3>Greek Salad</h3>
-                                <p className="price">$12.99</p>
-                            </div>
+        <div className="weekSpecialBox">
+            <div className="top">
+                <h1>This week's specials!</h1>
+                <button class="btn btn-warning">Online Menu</button>
+            </div>
+            <br />
+            <div className="content">
+                <div className="row">
+                    <div className="column">
+                        <div className="card">
+                            <img src={greek_salad} alt="cardImage" className="cardImage" />
+                            <div className="cardContent">
+                                <div className="cardHeading">
+                                    <h3>Greek Salad</h3>
+                                    <p className="price">$12.99</p>
+                                </div>
 
-                            <p className="description">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                                <p className="description">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
 
-                            <div className="order">
-                                <p>Order a Delivery</p>
-                                <img src={MdOutlineDeliveryDining} alt="Delivery" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src={bruschetta} alt="cardImage" className="cardImage" />
-                        <div className="cardContent">
-                            <div className="cardHeading">
-                                <h3>Bruschetta</h3>
-                                <p className="price">$5.99</p>
-                            </div>
-
-                            <p className="description">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
-
-                            <div className="order">
-                                <p>Order a Delivery</p>
-                                <img src={MdOutlineDeliveryDining} alt="Delivery" />
+                                <div className="order">
+                                    <p className="orderDel">Order a Delivery</p>
+                                    {/* <img src={BsFillCarFrontFill} alt="Delivery" /> */}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src={lemon_dessert} alt="cardImage" className="cardImage" />
-                        <div className="cardContent">
-                            <div className="cardHeading">
-                                <h3>Lemon Dessert</h3>
-                                <p className="price">$5.00</p>
-                            </div>
+                    
 
-                            <p className="description">This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                    <div className="column">
+                        <div className="card">
+                            <img src={bruschetta} alt="cardImage" className="cardImage" />
+                            <div className="cardContent">
+                                <div className="cardHeading">
+                                    <h3>Bruschetta</h3>
+                                    <p className="price">$5.99</p>
+                                </div>
 
-                            <div className="order">
-                                <p>Order a Delivery</p>
-                                <img src={MdOutlineDeliveryDining} alt="Delivery" />
+                                <p className="description">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                                <br />
+                                <div className="order">
+                                    <p className="orderDel">Order a Delivery</p>
+                                    {/* <img src={MdOutlineDeliveryDining} alt="Delivery" /> */}
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="column">
+                        <div className="card">
+                            <img height="171px" src={lemon_dessert} alt="cardImage" className="cardImage" />
+                            <div className="cardContent">
+                                <div className="cardHeading">
+                                    <h3>Lemon Dessert</h3>
+                                    <p className="price">$5.00</p>
+                                </div>
+                                <br />
+                                <p className="description">This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+
+                                <div className="order">
+                                    <p className="orderDel">Order a Delivery</p>
+                                    {/* <img src={MdOutlineDeliveryDining} alt="Delivery" /> */}
+                                </div>
+                            </div>
+                        </div>
+                        </div>
                 </div>
             </div>
         </div>
     </section>
 
-{/* //Testimonials */}
+{/* Testimonials */}
         <section id="testimonials">
             <div className="testContainer">
                 <h1>Testimonials</h1>
@@ -90,9 +95,9 @@ function Main() {
                             <h3 className="rating">Rating: 5.0</h3>
                             <div className="post">
                                 <img src={reviewer1} width="100px" alt="reviewer" />
-                                <p>Sarah</p>
+                                <p className="person">Sarah</p>
                             </div>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                            <p className="post">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
                         </div>
                     </div>
 
@@ -101,7 +106,7 @@ function Main() {
                             <h3 className="rating">Rating: 4.75</h3>
                             <div className="post">
                                 <img src={reviewer2} width="100px" alt="reviewer" />
-                                <p>Jack</p>
+                                <p className="person">Jack</p>
                             </div>
                             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
                         </div>
@@ -112,7 +117,7 @@ function Main() {
                             <h3 className="rating">Rating: 4.90</h3>
                             <div className="post">
                                 <img src={reviewer3} width="100px" alt="reviewer3" />
-                                <p>Jane</p>
+                                <p className="person">Jane</p>
                             </div>
                             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
                         </div>
@@ -123,36 +128,37 @@ function Main() {
                             <h3 className="rating">Rating: 4.45</h3>
                             <div className="post">
                                 <img src={reviewer4} width="100px" alt="reviewer4" />
-                                <p>Andrew</p>
+                                <p className="person">Andrew</p>
                             </div>
                             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
                         </div>
                     </div>
+                    
                 </div>
             </div>
-            </section>
+        </section>
 
 
         <section id="history">
-        <div className="history_section">
-            <div className="row">
-            <div className="col">
-                <h1>Little Lemon</h1>
-                <h1>Chicago</h1>
-                <br />
-                <br />
-                <p className="wordsTwo">Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment.  The restaurant features a locally-sourced menu with daily specials.  All started up years ago by Mario and Adrian.</p>
-            </div>
-            <div className="col">
-                <img width="300px" src= {Mario_and_Adrian_A}
-                alt="the owners of Little Lemon"
-                />
-                <img width="300px" src= {Mario_and_Adrian_B}
-                alt="the owners of Little Lemon"
-                />
+            <div className="history_section">
+                <div className="row">
+                <div className="col">
+                    <h1>Little Lemon</h1>
+                    <h1>Chicago</h1>
+                    <br />
+                    <br />
+                    <p className="wordsTwo">Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment.  The restaurant features a locally-sourced menu with daily specials.  All started up years ago by Mario and Adrian.</p>
+                </div>
+                <div className="col">
+                    <img width="300px" src= {Mario_and_Adrian_A}
+                    alt="the owners of Little Lemon"
+                    />
+                    <img width="300px" src= {Mario_and_Adrian_B}
+                    alt="the owners of Little Lemon"
+                    />
+                    </div>
                 </div>
             </div>
-        </div>
         </section>
     </main>
     )
